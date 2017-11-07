c5f2d8223c2ca5d707ca3e32379670613d25fe8f
Bhaktti <bhakti.gangras47@gmail.com>
2017-11-07 15:15:46 -0800

    First phone formatter assignment

---
+++ src/whileLoop.java
@@ -0,0 +1,38 @@
+/**
+ * Created by bhakti on 11/7/17.
+ */
+public class whileLoop {
+
+    public static void main (String[] args) {
+        int i=0;
+        int sum=0;
+       /* while (sum < 10){
+            i++;
+            sum =sum+i;
+            System.out.println(i + "and"+ sum);
+        }
+*/
+//infinite loop
+        /*while (sum < 10){
+            i++;
+            sum =sum-i;
+            System.out.println(i + "and"+ sum);
+        }*/
+
+        //No output
+        /*while (sum < 0){
+            i++;
+            sum =sum-i;
+            System.out.println(i + "and"+ sum);
+        }*/
+
+        //No output
+        /*while (sum >= 10){
+            i++;
+            sum =sum+i;
+            System.out.println(i + "and"+ sum);
+        }*/
+
+
+    }
+}
